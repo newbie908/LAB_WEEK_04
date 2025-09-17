9fa66d966fcd37ed83bec643dac755294702718d
Jansen Caesar <caesarjansen1@gmail.com>
2025-09-17 22:06:05 +0700

    Commit No. 1 – Navigation drawer

---
+++ app/src/main/java/com/example/lab_week_04/ListFragment.kt
@@ -0,0 +1,44 @@
+package com.example.lab_week_04
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val coffeeList = listOf(
+            view.findViewById<View>(R.id.affogato),
+            view.findViewById<View>(R.id.americano),
+            view.findViewById<View>(R.id.latte)
+        )
+
+        coffeeList.forEach { coffee ->
+            val fragmentBundle = Bundle()
+            fragmentBundle.putInt(COFFEE_ID, coffee.id)
+            coffee.setOnClickListener(
+                Navigation.createNavigateOnClickListener(
+                    R.id.action_listFragment_to_detailFragment,
+                    fragmentBundle
+                )
+            )
+        }
+    }
+
+    companion object {
+        const val COFFEE_ID = "COFFEE_ID"
+    }
+}
